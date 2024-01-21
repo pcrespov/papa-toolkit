5d7cb25aeabc6ae3322ac053914f01071f1940f9
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-01-21 18:17:04 +0100

    rename

--- mediakit/setup.py
+++ mediakit/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     packages=find_packages("src"),
-    name="image-syncer",
+    name="mediakit",
     version="0.1.0",
-    description="A script to organize images by their creation date.",
+    description="Tools to manage image and video files",
     author="pcrespov",
     license="MIT",
     python_requires=">=3.8",
